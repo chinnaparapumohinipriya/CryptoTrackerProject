3734c54e1faeba11ca7998fb7dde75d2c7045104
Mohini Priya <123290115+chinnaparapumohinipriya@users.noreply.github.com>
2023-01-22 21:37:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const form = document.querySelector('#searchForm');
 const res = document.querySelector('#resTable');
-const cont=document.getElementById("allContaint");
-var upd;
+const cont = document.getElementById("allContaint");
 
 form.addEventListener('submit',(e)=>{
-
     e.preventDefault();
-    const ctype=form.elements.coinType.value;
+    const ctype = form.elements.coinType.value;
     cont.classList.add('mainClick');
-    cont.classList.remove('main');
+    cont.classList.remove('main');    
     fetchPrice(ctype);
 
 });
@@ -17,36 +15,41 @@ const fetchPrice = async(ctype) =>{
     const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
     showPrice(r.data.coin);
 }
-const showprice=(coinData)=>{
-    const price=coinData.price;
-    const vol=coinData.volume;
-    const change=coinData.pricechange1d;
-    const coin=coinDataname;
-    const curr='USD';
-    var col="green";
+
+
+const showPrice = (coinData)=>{
+    const price = coinData.price;
+    const vol = coinData.volume;
+    const change = coinData.priceChange1d;
+    const coin = coinData.name;
+    const curr = 'USD';
+    var col= "green";
     var colo="red";
+
     if(change<0){
-        col="green";
+        col = "green";
     }
-   
-    res.innerHTML=`<tr class="bg-primary" style="color:white;">
+    res.innerHTML = `<tr class="bg-primary" style="color: white;">
     <td>
-        property
+        Property
     </td>
-    <td>value</td>
-</tr>
-<tr>
     <td>
-        ${coin}
-    </td>${col};"><span style="font-size: 1.3em;">${price}</span> ${curr}</td>
+        Value
+    </td>
 </tr>
 <tr>
-    <td> Volume(24hrs)</td>
-    <td style="color:${colo};>${vol}</td>
+    <td>${coin}</td>
+    <td style="color:${col};"><span style="font-size: 1.3em;">${price}</span> ${curr}</td>
 </tr>
 <tr>
-     <td> Change(24hrs)</td>
-     <td style="color:${col};">${change} ${curr}</td>
-</tr> `;
-};    
-        
+    <td>Volume (24hrs)</td>
+    <td  style="color:${colo};">${vol}</td>
+    
+</tr>
+<tr>
+    <td>Change (24hrs)</td>
+    <td  style="color:${col};">${change} ${curr}</td>
+
+    
+</tr>`;
+};
